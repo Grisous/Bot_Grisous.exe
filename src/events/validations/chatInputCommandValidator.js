43fd8a20b5bc6c4d35afeae5899401d9a414f468
Grisous <61704236+Grisous@users.noreply.github.com>
2024-12-06 11:42:28 +0100

    Avancement mineur

---
+++ src/events/validations/chatInputCommandValidator.js
@@ -0,0 +1,5 @@
+const {EmbedBuilder} = require('discord.js');
+const {developersID, testServerId} = require('../../config.json');
+const mConfig = require("../../messageConfig.json");
+const getLocalCommands = require("../../utils/getLocalCommands");
+
---
+++ src/utils/getLocalCommands.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const getAllFiles = require("./getAllFiles");
+
+module.exports = (exeception = []) => {
+  let localCommands = []
+  const commandCategories = getAllFiles(path.join(__dirname, "..", "commands"), true);
+
+  for (const commandCategory of commandCategories) {
+    const commandsFiles = getAllFiles(commandCategory);
+
+    for (const commandsFile of commandsFiles) {
+      const commandObject = require(commandsFile);
+
+      if (exeception.includes(commandObject.name)) continue;
+      localCommands.push(commandObject);
+    }
+  }
+}
