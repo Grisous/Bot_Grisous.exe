cb2bd42a4a45704a2b5d08a214acca1b48362a92
Grisous <lechatjoker.ytb@gmail.com>
2025-03-18 15:49:31 +0100

    Ajout du code pour \buttons\punishmentBtn

---
+++ src/buttons/punishmentBtn.js
@@ -0,0 +1,70 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  Component,
+} = require("discord.js");
+
+module.exports = {
+  customId: "punishmentBtn",
+  userPermissions: [],
+  botPermissions: [],
+
+  run: async (client, interaction) => {
+    const { message, channel, guildId, guild, user } = interaction;
+
+    await interaction.deferReply({ ephemeral: true });
+
+    try {
+      const embedAuthor = message.embeds[0].author;
+      const targetMember = await guild.members
+        .fetch({
+          query: embedAuthor.name,
+          limit: 1,
+        })
+        .first();
+
+      const Oembed = new EmbedBuilder()
+        .setTitle("Sanctions")
+        .setAuthor({
+          name: `${targetMember.user.username}`,
+          iconURL: `${targetMember.user.displayAvatarURL({ dynamic: true })}`,
+        })
+        .setDescription(
+          `\`❔\` Quelle sanction veux-tu appliquer sur ${targetMember.user.username} ?`
+        );
+
+      const otherRow = new ActionRowBuilder().setComponents(
+        new ButtonBuilder()
+          .setCustomId("banBtn")
+          .setLabel("Bannir du sreveur")
+          .setStyle(ButtonStyle.Danger),
+        new ButtonBuilder()
+          .setCustomId("kickBtn")
+          .setStyle(ButtonStyle.Danger)
+          .setLabel("kick"),
+        new ButtonBuilder()
+          .setCustomId("tempmuteBtn")
+          .setLabel("Exclure (Mute) temporairement")
+          .setStyle(ButtonStyle.Danger),
+        new ButtonBuilder()
+          .setCustomId("tempbanBtn")
+          .setLabel("Bannir temporairement")
+          .setStyle(ButtonStyle.Danger),
+        new ButtonBuilder()
+          .setCustomId("cancelBtn")
+          .setLabel("Annuler")
+          .setStyle(ButtonStyle.Secondary)
+      );
+
+      await interaction.editReply({ embeds: [Oembed], components: [otherRow] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content:
+          "Une erreur est survenue lors de l'affichage du modal. \n Veuillez en informer @Grisous, mon développer avec l'heure exacte d'apparition de l'erreur.",
+        ephemeral: true,
+      });
+    }
+  },
+};
