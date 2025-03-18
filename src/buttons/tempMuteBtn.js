e27309882bdefb9ca31a86581878727ce022375d
Grisous <lechatjoker.ytb@gmail.com>
2025-03-18 15:50:02 +0100

    Ajout du code pour \buttons\tempmuteBtn

---
+++ src/buttons/tempMuteBtn.js
@@ -0,0 +1,50 @@
+const {
+  PermissionFlagsBits,
+  ModalBuilder,
+  ActionRowBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} = require("discord.js");
+
+module.exports = {
+  customId: "tempMuteBtn",
+  userPermissions: [PermissionFlagsBits.ModerateMembers],
+  botPermissions: [PermissionFlagsBits.ModerateMembers],
+
+  run: async (client, interaction) => {
+    try {
+      const tempMuteModal = new ModalBuilder()
+        .setTitle("Temp Mute - Exclure temporairement")
+        .setCustomId("tempmuteMdl")
+        .setComponents(
+          new ActionRowBuilder().setComponents(
+            new TextInputBuilder()
+              .setLabel("Durée")
+              .setCustomId("tempmuteTime")
+              .setPlaceholderText(
+                "Durée en minute par défaut, `h` pour heure, `j` pour jour, `m` pour mois"
+              )
+              .setStyle(TextInputStyle.Short)
+          ),
+          new ActionRowBuilder().setComponents(
+            new TextInputBuilder()
+              .setLabel("Raison")
+              .setCustomId("tempmuteReason")
+              .setPlaceholderText(
+                "Indiquez la raison de l'expulsion temporaire"
+              )
+              .setStyle(TextInputStyle.Paragraph)
+          )
+        );
+
+      return await interaction.showModal(tempMuteModal);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content:
+          "Une erreur est survenue lors de l'affichage du modal. \n Veuillez en informer @Grisous, mon développer avec l'heure exacte d'apparition de l'erreur.",
+        ephemeral: true,
+      });
+    }
+  },
+};
