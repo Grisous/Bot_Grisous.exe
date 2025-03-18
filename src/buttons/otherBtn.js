bcfdb7bbc3ff1bde5f5d5f8d4b3c8f68d9d3a352
Grisous <lechatjoker.ytb@gmail.com>
2025-03-18 15:49:18 +0100

    Ajout du code pour \buttons\otherBtn

---
+++ src/buttons/otherBtn.js
@@ -0,0 +1,62 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonStyle,
+  Component,
+} = require("discord.js");
+
+module.exports = {
+  customId: "otherBtn",
+  userPermissions: [],
+  botPermissions: [],
+
+  run: async (client, interaction) => {
+    const { message, channel, guildId, guild, user } = interaction;
+
+    await interaction.deferReply({ ephemeral: true });
+
+    try {
+      const embedAuthor = message.embeds[0].author;
+      const targetMember = await guild.members
+        .fetch({
+          query: embedAuthor.name,
+          limit: 1,
+        })
+        .first();
+
+      const Oembed = new EmbedBuilder()
+        .setTitle("Autres options")
+        .setAuthor({
+          name: `${targetMember.user.username}`,
+          iconURL: `${targetMember.user.displayAvatarURL({ dynamic: true })}`,
+        })
+        .setDescription(
+          `\`❔\` Quelle action veux-tu effectuer sur ${targetMember.user.username} ?`
+        );
+
+      const otherRow = new ActionRowBuilder().setComponents(
+        new ButtonBuilder()
+          .setCustomId("renameBtn")
+          .setLabel("Renommer")
+          .setStyle(ButtonStyle.Primary),
+        new ButtonBuilder()
+          .setCustomId("addroleBtn")
+          .setLabel("Ajouter un role")
+          .setStyle(ButtonStyle.Danger),
+        new ButtonBuilder()
+          .setCustomId("cancelBtn")
+          .setLabel("Annuler")
+          .setStyle(ButtonStyle.Secondary)
+      );
+
+      await interaction.editReply({ embeds: [Oembed], components: [otherRow] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({
+        content:
+          "Une erreur est survenue lors de l'affichage du modal. \n Veuillez en informer @Grisous, mon développer avec l'heure exacte d'apparition de l'erreur.",
+        ephemeral: true,
+      });
+    }
+  },
+};
